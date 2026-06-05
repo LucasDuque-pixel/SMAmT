f200d012be7e300c519caa6f33ee50389c4a3fba
LucasDuque <lucasduque.ma@gmail.com>
2026-06-05 20:14:21 -0300

    Ajustes no IP.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 const char* ssid = "AndroidLucas";
 const char* password = "kags1111";
-const char* serverName = "http://10.120.183.218:5000/dados"; 
+const char* serverName = "http://10.174.40.218:5000/dados"; 
 
 #define DHTPIN 4 
 #define DHTTYPE DHT11 
